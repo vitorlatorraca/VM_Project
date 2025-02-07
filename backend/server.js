4fc488b813183d2e4c1048a9228598ae7657bd7c
vitorlatorraca <vitorlatorraca5@gmail.com>
2025-02-07 16:06:09 -0500

    new

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(() => console.log("✅ Conectado ao MongoDB!"))
+  .then(() => console.log("✅ Conectado ao MongoDB!!"))
   .catch(err => {
     console.error("❌ Erro ao conectar ao MongoDB:", err);
     process.exit(1); // Encerra o servidor em caso de falha na conexão
