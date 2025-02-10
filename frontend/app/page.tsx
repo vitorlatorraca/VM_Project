95971925a42caf73fe95b6be4b69882bb4a7c558
vitorlatorraca <vitorlatorraca5@gmail.com>
2025-02-09 23:08:42 -0500

    new

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Game from "@/components/Game";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-sans">
-      <h1 className="text-3xl font-bold mb-6">MagoNegroGame</h1>
+      <h1 className="text-3xl font-bold mb-6">MagoNegroGames.</h1>
       <Game />
     </div>
   );
