da40566e64e3d762dda2e7bca1b1bf99dbfc5053
vitorlatorraca <vitorlatorraca5@gmail.com>
2025-03-06 00:05:58 -0500

    new

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  score: { type: Number, default: 0 }, // <-- Novo campo,
+  score: { type: Number, default: 0 }, // <-- Novo campo
   profilePic: {type: String, default: ""},
 });
 
