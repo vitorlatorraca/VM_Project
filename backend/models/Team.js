00c53df4243ca13005879764d63d841f7d2b73f9
vitorlatorraca <vitorlatorraca5@gmail.com>
2025-03-10 23:04:22 -0400

    new

--- backend/models/Team.js
+++ backend/models/Team.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  logoPath: { type: String, required: true }, // Caminho da imagem do escudo
+  logoPath: { type: String, required: true }, // Caminho da imagem do escudo.
 });
 
 const Team = mongoose.model("Team", teamSchema);
